799600dd4ab7cc8ff90167e2bde716322d1063f2
Samarth Desai <37949934+SamarthDesai01@users.noreply.github.com>
2018-06-15 16:08:48 -0500

    Add files via upload

---
+++ convert.js
@@ -0,0 +1,33 @@
+const readline = require('readline');
+const fs = require('fs');
+
+const variableName = 'test';
+const variableType = 'const';
+const outputFileName = 'output.js';
+const inputFileName = 'nounlist.txt';
+
+var outputStream = fs.createWriteStream(outputFileName, {flags: 'a'});
+
+//Declare our variable
+outputStream.write(`${variableType} ${variableName} = [`);
+
+//Create the stream to read the file from
+const inputStream = readline.createInterface({
+    input: fs.createReadStream(inputFileName)
+});
+
+//Add each line as an item to the array
+inputStream.on('line', (line) => {
+    if(line.indexOf('\'') === -1){
+        outputStream.write(`\'${line}\',`);
+    }
+});
+
+//Close the array and export it
+inputStream.on('close', () => {
+    outputStream.write(']');
+    outputStream.write("\n\nmodule.exports = {");
+    outputStream.write("\n    " + variableName);
+    outputStream.write("\n};");
+});
+
